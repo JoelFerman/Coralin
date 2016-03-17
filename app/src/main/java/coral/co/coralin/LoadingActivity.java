304caf7b1a21b9505a57b1fb86eb47f6daca9550
JoelFerman <joelferman@hotmail.com>
2016-03-17 15:40:29 -0600

    error on project

--- app/src/main/java/coral/co/coralin/LoadingActivity.java
+++ app/src/main/java/coral/co/coralin/LoadingActivity.java
@@ -1,17 +1,14 @@
 package coral.co.coralin;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class LoadingActivity extends AppCompatActivity implements View.OnClickListener
+public class LoadingActivity extends AppCompatActivity implements OnClickListener
 {
-    GlobalVars GV = new GlobalVars();
-
     Button btnContinue;
 
     @Override
@@ -19,9 +16,6 @@ public class LoadingActivity extends AppCompatActivity implements View.OnClickLi
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-
-//        SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-//        String age = sharedPreferences.getString("Age", "");
 
         btnContinue = (Button) findViewById(R.id.btnContinue);
 
@@ -34,25 +28,9 @@ public class LoadingActivity extends AppCompatActivity implements View.OnClickLi
         switch (v.getId())
         {
             case R.id.btnContinue:
-
-//                if (GV.tutorialView == 0)
-//                {
-                    Intent goTutorial = new Intent(this, AgeVerificationActivity.class);
-                    finish();
-                    startActivity(goTutorial);
-//                }
-//                else if (GV.tutorialView == 1)
-//                {
-//                    Intent goAgeVerification = new Intent(this, AgeVerificationActivity.class);
-//                    finish();
-//                    startActivity(goAgeVerification);
-//                }
-//                else if (GV.tutorialView == 2)
-//                {
-//                    Intent goLogin = new Intent(this, LoginActivity.class);
-//                    finish();
-//                    startActivity(goLogin);
-//                }
+                Intent goTutorial = new Intent(this, AgeVerificationActivity.class);
+                finish();
+                startActivity(goTutorial);
                 break;
         }
     }
