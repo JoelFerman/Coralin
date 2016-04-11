8c483cc5ac55ff80e2b4537d8b07af353ed29303
JoelFerman <joelferman@hotmail.com>
2016-04-11 16:22:20 -0600

    MainScreen Fragments
    Fragments added to app, commit to merge with backend

---
+++ app/src/main/java/coral/co/coralin/MyDownloadedGamesFragment.java
@@ -0,0 +1,23 @@
+package coral.co.coralin;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by joelferman on 4/11/16.
+ */
+public class MyDownloadedGamesFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_my_downloaded_games,container,false);
+        return v;
+    }
+}
